76b11ee6b5304827b5cae0de5e053a52a21b0ac2
abhishek7071 <38905469+abhishek7071@users.noreply.github.com>
2021-01-31 08:53:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,18 @@ from datetime import datetime
 import re
 import requests as r
 from bs4 import BeautifulSoup as bs
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+#import os
+chrome_options = Options()
+options = Options()
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=chrome_options)
+gmail_id="aman765180@gmail.com"
 app = Flask(__name__)
 global stock3
 @app.route("/", methods=['GET','POST'])
